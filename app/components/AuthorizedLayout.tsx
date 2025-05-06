9dba1b0481053f4b96e96db1fb94a3839290bc15
Va7ul <vas7ul@gmail.com>
2025-05-06 21:28:41 +0300

    Update AuthorizedLayout.tsx

--- app/components/AuthorizedLayout.tsx
+++ app/components/AuthorizedLayout.tsx
@@ -4,7 +4,7 @@ type Props = {
 
 export const AuthorizedLayout = ({ children }: Props) => {
   return (
-    <div className="flex flex-col gap-2.5 px-5 pb-10 md:gap-4 md:px-8 md:pb-8 xl:flex-row">
+    <div className="mx-auto flex max-w-[1216px] flex-col gap-2.5 px-5 pb-10 md:gap-4 md:px-8 md:pb-8 xl:flex-row">
       {children}
     </div>
   );
