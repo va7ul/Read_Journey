1c7fa09ea066134c42cbb4d1ac70e7e23fe7b7d1
Va7ul <vas7ul@gmail.com>
2025-05-06 21:28:32 +0300

    Update Dashboard.tsx

--- app/components/Dashboard.tsx
+++ app/components/Dashboard.tsx
@@ -4,10 +4,8 @@ type Props = {
 
 export const Dashboard = ({ children }: Props) => {
   return (
-    <div>
-      <div className="bg-black-secondary flex flex-col gap-x-8 rounded-[30px] p-5 md:p-8 md:max-xl:flex-row xl:p-5 xl:pt-10">
-        {children}
-      </div>
+    <div className="bg-black-secondary flex flex-col gap-x-8 rounded-[30px] p-5 md:p-8 md:max-xl:flex-row xl:p-5 xl:pt-10">
+      {children}
     </div>
   );
 };
