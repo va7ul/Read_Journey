e4acd3c3669e5ea0ec2b31546fb1fbc935566ee4
Va7ul <vas7ul@gmail.com>
2025-05-14 19:50:34 +0300

    Update Navigation.tsx

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -1,8 +1,9 @@
 'use client';
 
-import clsx from 'clsx';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+
+import clsx from 'clsx';
 
 type Props = {
   onClose?: () => void;
